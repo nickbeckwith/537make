03231fc317ea2538b5cb99b8275e87b9150b3f07
Nicholas Beckwith <nbeckwith2@wisc.edu>
2018-11-01 23:31:12 -0500

    Changes directory and adds more to parser

--- src/parser.c
+++ src/parser.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include "parser.h"
+#include "build_spec.h"
+#include "tools.h"
 
 build_list_t * readAll(char *filename) {
 	build_list_t * return_file;
-	FILE * file_pointer;
+	FILE * fptr;
 }
 
+/**
+ * Assumes this is called while fptr is on first column
+ * Advances fptr to target and consumes "target:"
+ * and returns "target"
+ */
+char * getTarget(FILE *fptr) {
+	char *target = (char *) mallocWrapper(MAX_FILE_SIZE * sizeof(char));
+	fscanf(fptr, "%s:", target);
+	return target;
+	//TODO ensure that fscanf only gets target if target is on first column
+}
+
+/**
+ * Get dependents. Assumes fptr is currently on char after ':'
+ * Returns a single cmd and advances past single cmd returns null
+ * if there are no more commands left
+ */
+char * getCmd(FILE *fptr) {
+	char *cmd = (char *) mallocWrapper(MAX_SIZE_OTHER * sizeof(char));
+	fscanf(fptr, "%s", cmd);
+	return cmd;
+}
+
+
+// TODO Write fscanf wrapper that takes fscanf output as input and 
+// does error handling. i.e. scanErr(fscanf(...));
--- src/test_main.cpp
+++ src/test_main.cpp
@@ -4,4 +4,4 @@
 // Let Catch provide main():
 #define CATCH_CONFIG_MAIN
 
-#include <catch2/catch.hpp>
+#include "catch.hpp"
