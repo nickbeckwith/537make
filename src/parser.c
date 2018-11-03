f4cf2098262552486688cecf51f7e801bc3d5710
strimaitis <33992296+strimaitis@users.noreply.github.com>
2018-11-03 15:32:46 -0500

    Update parser.c

--- src/parser.c
+++ src/parser.c
@@ -4,9 +4,26 @@
 #include "tools.h"
 
 build_list_t * readAll(char *filename) {
-	build_list_t * return_file;
-	FILE * fptr;
+        build_list_t * return_file;
+        FILE * file_pointer;
+        // char * temp_char_ptr
+        return_file->target = getTarget(file_pointer);
+
+        while(getCmd(file_pointer) != NULL) {
+                ((return_file->cmds)* + return_file->cmds_len)* = getCmd(file_pointer);
+                // MAY NOT NEED TO INCLUDE, DEPENDING OTHER FUNCTIONS
+                return_file->cmds_len++;
+        }
+
+        while(getDep(file_pointer) != NULL) {
+                ((return_file->dependents)* + return_file->dependents_len)* = getDep(file_pointer);
+                // MAY NOT NEED TO INCLUDE, DEPENDING OTHER FUNCTIONS
+		return_file->dependents_len++;
+        }
+
+
 }
+
 
 /**
  * Assumes this is called while fptr is on first column
@@ -20,11 +37,12 @@ char * getTarget(FILE *fptr) {
 	//TODO ensure that fscanf only gets target if target is on first column
 }
 
-/**
- * Get dependents. Assumes fptr is currently on char after ':'
- * Returns a single cmd and advances past single cmd returns null
- * if there are no more commands left
- */
+char * getDep(FILE *fptr) {
+        char *cmd = (char *) mallocWrapper(MAX_OTHERS_LEN * sizeof(char));
+        fscanf(fptr, "%s", cmd);
+        return cmd;
+}
+
 char * getCmd(FILE *fptr) {
 	char *cmd = (char *) mallocWrapper(MAX_SIZE_OTHER * sizeof(char));
 	fscanf(fptr, "%s", cmd);
